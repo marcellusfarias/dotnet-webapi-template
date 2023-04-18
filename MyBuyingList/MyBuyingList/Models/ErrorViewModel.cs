7872dc7c30e53a69f1712e0020638551c6a353aa
Marcellus Farias <mafarias@concordservicing.com>
2023-04-17 23:09:11 -0300

    First commit, using Clean Architecture

---
+++ MyBuyingList/MyBuyingList/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyBuyingList.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
