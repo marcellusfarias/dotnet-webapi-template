f437621d48a2d97f127e6bd07638b03c50a6b3da
Marcellus Farias <mafarias@concordservicing.com>
2023-04-18 19:02:03 -0300

    Added base files to Domain and other configurations

---
+++ MyBuyingList/MyBuyingList.Domain/Common/BaseAuditableEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBuyingList.Domain.Common;
+
+public abstract class BaseAuditableEntity : BaseEntity
+{
+    public DateTime Created { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModified { get; set; }
+    public string? LastModifiedBy { get; set; }
+}
