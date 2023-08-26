dfed83912a1d7a0ecda25954fa3e19ce4c0ca915
Marcellus Farias <marcelluscfarias@gmail.com>
2023-08-26 15:05:07 -0300

    Fixed some tests, but there is more to be done

---
+++ MyBuyingList/MyBuyingList.Application.Tests/MapperHelper.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MyBuyingList.Application.Common.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBuyingList.Application.Tests
+{
+    internal static class MapperHelper
+    {
+        public static IMapper GetMapper()
+        {
+            MapperConfiguration mapperConfiguration = new MapperConfiguration(cfg =>
+            {
+                var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var assembly = assemblies.Single(x => x.ManifestModule.Name.Equals("MyBuyingList.Application.dll"));
+
+                var types = assembly.GetTypes()
+                        .Where(type => type.GetCustomAttributes(typeof(AutoMapperMappingAttribute), true).Any());
+
+                foreach (var mappingType in types)
+                {
+                    var instance = Activator.CreateInstance(mappingType);
+                    var method = mappingType.GetMethod("ConfigureMappings"); 
+                    method?.Invoke(instance, new object[] { cfg });
+                }
+            });
+
+            return mapperConfiguration.CreateMapper();
+        }        
+    }
+}
--- MyBuyingList/MyBuyingList.Application.Tests/UnitTests/UserServiceTests.cs
+++ MyBuyingList/MyBuyingList.Application.Tests/UnitTests/UserServiceTests.cs
@@ -1,210 +1,206 @@
-//using AutoMapper;
-//using FluentAssertions;
-//using FluentValidation;
-//using Moq;
-//using MyBuyingList.Application.Common.Exceptions;
-//using MyBuyingList.Application.Common.Interfaces.Repositories;
-//using MyBuyingList.Application.DTOs;
-//using MyBuyingList.Application.Services;
-//using MyBuyingList.Domain.Entities;
-//using MyBuyingList.Application.Common.Mappings;
+using AutoMapper;
+using FluentAssertions;
+using FluentValidation;
+using Moq;
+using MyBuyingList.Application.Common.Exceptions;
+using MyBuyingList.Application.Common.Interfaces.Repositories;
+using MyBuyingList.Application.DTOs;
+using MyBuyingList.Application.Services;
+using MyBuyingList.Domain.Entities;
+using MyBuyingList.Application.Common.Mappings;
+using MyBuyingList.Application.DTOs.UserDtos;
 
-//namespace MyBuyingList.Application.Tests.UnitTests;
+namespace MyBuyingList.Application.Tests.UnitTests;
 
-//public class UserServiceTests
-//{
-//    private UserService _sut;
-//    private readonly Mock<IUserRepository> _userRepositoryMock = new Mock<IUserRepository>();
-//    private readonly IMapper _mapper;
-//    private readonly IValidator<UserDto> _validatorMock = new UserValidator(); //should not mock: https://docs.fluentvalidation.net/en/latest/testing.html#
+public class UserServiceTests
+{
+    private UserService _sut;
+    private readonly Mock<IUserRepository> _userRepositoryMock = new Mock<IUserRepository>();
+    private readonly IMapper _mapper;
 
-//    public UserServiceTests()
-//    {
-//        //var mappingConfig = new MapperConfiguration(mc =>
-//        //{
-//        //    mc.AddProfile(new DefaultProfile());
-//        //});
+    public UserServiceTests()
+    {
+        _mapper = MapperHelper.GetMapper();
+        _sut = new UserService(_userRepositoryMock.Object, _mapper);
+    }
 
-//        //_mapper = mappingConfig.CreateMapper();
+    [Fact]
+    public void GetAllUsers_ShouldReturnDtoList_WhenThereAreUsers()
+    {
+        //Arrange
+        User user1 = new User() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Password = "123", Active = true };
+        User user2 = new User() { Id = 2, UserName = "Bob", Email = "bob@gmail.com", Password = "321", Active = true };
+        User user3 = new User() { Id = 3, UserName = "Cindy", Email = "cindy@outlook.com", Password = "cindy123", Active = false };
 
-//        //_sut = new UserService(_userRepositoryMock.Object, _mapper, _validatorMock);
-//    }
+        IEnumerable<User> mockedUsers = new List<User>() { user1, user2, user3 };
+        _userRepositoryMock
+            .Setup(x => x.GetAll())
+            .Returns(mockedUsers);
 
-//    [Fact]
-//    public void List_ShouldReturnDtoList_WhenThereAreUsers()
-//    {
-//        //Arrange
-//        //User user1 = new User() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Password = "123", Active = true };
-//        //User user2 = new User() { Id = 2, UserName = "Bob", Email = "bob@gmail.com", Password = "321", Active = true };
-//        //User user3 = new User() { Id = 3, UserName = "Cindy", Email = "cindy@outlook.com", Password = "cindy123", Active = false };
+        //Act
+        IEnumerable<GetUserDto> returnUsers = _sut.GetAllUsers();
 
-//        //IEnumerable<User> mockedUsers = new List<User>() { user1, user2, user3 };
-//        //_userRepositoryMock
-//        //    .Setup(x => x.GetAll())
-//        //    .Returns(mockedUsers);
+        //Assert
+        GetUserDto userDto1 = new GetUserDto() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Active = true };
+        GetUserDto userDto2 = new GetUserDto() { Id = 2, UserName = "Bob", Email = "bob@gmail.com", Active = true };
+        GetUserDto userDto3 = new GetUserDto() { Id = 3, UserName = "Cindy", Email = "cindy@outlook.com", Active = false };
+        IEnumerable<GetUserDto> expectedUserDtos = new List<GetUserDto>() { userDto1, userDto2, userDto3 };
 
-//        ////Act
-//        //IEnumerable<UserDto> returnUsers = _sut.GetAllUsers();
+        returnUsers.Should().BeEquivalentTo(expectedUserDtos);
+    }
 
-//        ////Assert
-//        //UserDto userDto1 = new UserDto() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Active = true };
-//        //UserDto userDto2 = new UserDto() { Id = 2, UserName = "Bob", Email = "bob@gmail.com", Active = true };
-//        //UserDto userDto3 = new UserDto() { Id = 3, UserName = "Cindy", Email = "cindy@outlook.com", Active = false };
-//        //IEnumerable<UserDto> expectedUserDtos = new List<UserDto>() { userDto1, userDto2, userDto3 };
+    [Fact]
+    public void GetAllUsers_ShouldReturnEmptyList_WhenThereAreNoUsers()
+    {
+        //Arrange
+        _userRepositoryMock
+            .Setup(x => x.GetAll())
+            .Returns(Enumerable.Empty<User>());
 
-//        //returnUsers.Should().BeEquivalentTo(expectedUserDtos);
-//    }
+        //Act
+        var users = _sut.GetAllUsers();
 
-//    [Fact]
-//    public void List_ShouldReturnEmptyList_WhenThereAreNoUsers()
-//    {
-//        ////Arrange
-//        //_userRepositoryMock
-//        //    .Setup(x => x.GetAll())
-//        //    .Returns(Enumerable.Empty<User>());
+        //Assert
+        users.Should().BeEmpty();
+    }
 
-//        ////Act
-//        //var users = _sut.List();
+    public static IEnumerable<object[]> ValidDtos =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                new User() { UserName = "Alph", Email = "alph@hotmail.com", Password = "123", Active = true },
+                new CreateUserDto() { UserName = "Alph", Email = "alph@hotmail.com", Password = "123" },
+            },
+            new object[]
+            {
+                new User() { UserName = "Bob", Email = "bob@gmail.com", Password = "123", Active = false },
+                new CreateUserDto() {  UserName = "Bob", Email = "bob@gmail.com", Password = "123" },
+            },
+            new object[]
+            {
+                new User() { UserName = "Cindy McAdams", Email = "cindy@gmail.com", Password = "123", Active = false },
+                new CreateUserDto() { UserName = "Cindy McAdams", Email = "cindy@gmail.com", Password = "123" },
+            },
+        };
 
-//        ////Assert
-//        //users.Should().BeEmpty();
-//    }
+    //public static IEnumerable<object[]> InvalidDtos =>
+    //    new List<object[]>
+    //    {
+    //        new object[]
+    //        {
+    //            new UserDto() { Id = 1, UserName = "", Email = "myemail@email.com", Active = true },
+    //            "An error occured while validating the model. Exception: Validation failed: \r\n -- UserName: Please specify a name. Severity: Error"
+    //        },
+    //        new object[]
+    //        {
+    //            new UserDto() { Id = 1, UserName = "John", Email = string.Empty, Active = true },
+    //            "An error occured while validating the model. Exception: Validation failed: \r\n -- Email: Please specify an email address. Severity: Error"
+    //        },
+    //        new object[]
+    //        {
+    //            new UserDto() { Id = 1, UserName = "John", Email = "myemail@email.com", Active = true },
+    //            "An error occured while validating the model. Exception: Validation failed: \r\n -- Password: Please specify a password. Severity: Error"
+    //        },
+    //        new object[]
+    //        {
+    //            new UserDto() { Id = -1, UserName = "John", Email = "myemail@email.com", Active = false },
+    //            "An error occured while validating the model. Exception: Validation failed: \r\n -- Id: Id should have a positive value. Severity: Error"
+    //        }
+    //};
 
-//    public static IEnumerable<object[]> ValidDtos =>
-//        new List<object[]>
-//        {
-//            new object[]
-//            {
-//                new User() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Password = "123", Active = true },
-//                new UserDto() { Id = 1, UserName = "Alph", Email = "alph@hotmail.com", Active = true },
-//            },
-//            new object[]
-//            {
-//                new User() { Id = 1000, UserName = "Bob", Email = "bob@gmail.com", Password = "123", Active = false },
-//                new UserDto() { Id = 1000, UserName = "Bob", Email = "bob@gmail.com",  Active = false },
-//            },
-//            new object[]
-//            {
-//                new User() { Id = 10, UserName = "Cindy McAdams", Email = "cindy@gmail.com", Password = "123", Active = false },
-//                new UserDto() { Id = 10, UserName = "Cindy McAdams", Email = "cindy@gmail.com", Active = false },
-//            },
-//        };
+    ////TODO: try to use this: 
+    //// https://stackoverflow.com/questions/54219742/mocking-ef-core-dbcontext-and-dbset
+    //[Theory]
+    //[MemberData(nameof(ValidDtos))]
+    //public void Create_ShouldHaveNoErrors_WhenDtoIsValid(User user, CreateUserDto userDto)
+    //{
+    //    //Arrange
+    //    _userRepositoryMock
+    //        .Setup(x => x.Add(It.IsAny<User>()))
+    //        .Returns(() => 1);
 
-//    public static IEnumerable<object[]> InvalidDtos =>
-//        new List<object[]>
-//        {
-//            new object[]
-//            {
-//                new UserDto() { Id = 1, UserName = "", Email = "myemail@email.com", Active = true },
-//                "An error occured while validating the model. Exception: Validation failed: \r\n -- UserName: Please specify a name. Severity: Error"
-//            },
-//            new object[]
-//            {
-//                new UserDto() { Id = 1, UserName = "John", Email = string.Empty, Active = true },
-//                "An error occured while validating the model. Exception: Validation failed: \r\n -- Email: Please specify an email address. Severity: Error"
-//            },
-//            new object[]
-//            {
-//                new UserDto() { Id = 1, UserName = "John", Email = "myemail@email.com", Active = true },
-//                "An error occured while validating the model. Exception: Validation failed: \r\n -- Password: Please specify a password. Severity: Error"
-//            },
-//            new object[]
-//            {
-//                new UserDto() { Id = -1, UserName = "John", Email = "myemail@email.com", Active = false },
-//                "An error occured while validating the model. Exception: Validation failed: \r\n -- Id: Id should have a positive value. Severity: Error"
-//            }
-//        };
+    //    //Act
+    //    var createdId = _sut.Create(userDto);
 
-//    //TODO: try to use this: 
-//    // https://stackoverflow.com/questions/54219742/mocking-ef-core-dbcontext-and-dbset
-//    [Theory]
-//    [MemberData(nameof(ValidDtos))]
-//    public void Create_ShouldHaveNoErrors_WhenDtoIsValid(User user, UserDto userDto)
-//    {
-//        //Arrange
-//        //_userRepositoryMock
-//        //    .Setup(x => x.Add(user));
+    //    //Assert
+    //    _userRepositoryMock.Verify(r => r.Add(It.IsAny<User>()), Times.Once);
+    //    createdId.Should().Be(1);
+    //}
 
-//        ////Act
-//        //_sut.Create(userDto);
+    ////TODO: pass as parameter many DTO with different errors.
+    //[Theory]
+    //[MemberData(nameof(InvalidDtos))]
+    //public void Create_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
+    //{
+    //    //Arrange
 
-//        ////Assert
-//        //_userRepositoryMock.Verify(r => r.Add(It.IsAny<User>()), Times.Once);
-//    }
+    //    //Act
+    //    //var action = _sut.Invoking(x => _sut.Create(newUser));
 
-//    //TODO: pass as parameter many DTO with different errors.
-//    [Theory]
-//    [MemberData(nameof(InvalidDtos))]
-//    public void Create_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
-//    {
-//        //Arrange
+    //    //// Assert
+    //    //action.Should()
+    //    //    .Throw<CustomValidationException>()
+    //    //    .WithMessage(errorMessage);
+    //}
 
-//        //Act
-//        //var action = _sut.Invoking(x => _sut.Create(newUser));
+    //[Theory]
+    //[MemberData(nameof(ValidDtos))]
+    //public void Update_ShouldHaveNoErrors_WhenDtoIsValid(User user, UserDto userDto)
+    //{
+    //    //Arrange
+    //    _userRepositoryMock
+    //        .Setup(x => x.Edit(user));
 
-//        //// Assert
-//        //action.Should()
-//        //    .Throw<CustomValidationException>()
-//        //    .WithMessage(errorMessage);
-//    }
+    //    //Act
+    //    //_sut.Update(userDto);
 
-//    [Theory]
-//    [MemberData(nameof(ValidDtos))]
-//    public void Update_ShouldHaveNoErrors_WhenDtoIsValid(User user, UserDto userDto)
-//    {
-//        //Arrange
-//        _userRepositoryMock
-//            .Setup(x => x.Edit(user));
+    //    //// Assert
+    //    //_userRepositoryMock.Verify(r => r.Edit(It.IsAny<User>()), Times.Once);
+    //}
 
-//        //Act
-//        //_sut.Update(userDto);
+    //[Theory]
+    //[MemberData(nameof(InvalidDtos))]
+    //public void Update_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
+    //{
+    //    //Arrange
 
-//        //// Assert
-//        //_userRepositoryMock.Verify(r => r.Edit(It.IsAny<User>()), Times.Once);
-//    }
+    //    //Act
+    //    //var action = _sut.Invoking(x => _sut.Update(newUser));
 
-//    [Theory]
-//    [MemberData(nameof(InvalidDtos))]
-//    public void Update_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
-//    {
-//        //Arrange
-       
-//        //Act
-//        //var action = _sut.Invoking(x => _sut.Update(newUser));
+    //    //// Assert
+    //    //action.Should()
+    //    //    .Throw<CustomValidationException>()
+    //    //    .WithMessage(errorMessage);
+    //}
 
-//        //// Assert
-//        //action.Should()
-//        //    .Throw<CustomValidationException>()
-//        //    .WithMessage(errorMessage);
-//    }
+    //[Theory]
+    //[MemberData(nameof(ValidDtos))]
+    //public void Delete_ShouldHaveNoErrors_WhenDtoIsValid(User user, UserDto userDto)
+    //{
+    //    //Arrange
+    //    _userRepositoryMock
+    //        .Setup(x => x.Delete(user));
 
-//    [Theory]
-//    [MemberData(nameof(ValidDtos))]
-//    public void Delete_ShouldHaveNoErrors_WhenDtoIsValid(User user, UserDto userDto)
-//    {
-//        //Arrange
-//        _userRepositoryMock
-//            .Setup(x => x.Delete(user));
+    //    ////Act
+    //    //_sut.Delete(userDto);
 
-//        ////Act
-//        //_sut.Delete(userDto);
+    //    //// Assert
+    //    //_userRepositoryMock.Verify(r => r.Delete(It.IsAny<User>()), Times.Once);
+    //}
 
-//        //// Assert
-//        //_userRepositoryMock.Verify(r => r.Delete(It.IsAny<User>()), Times.Once);
-//    }
+    //[Theory]
+    //[MemberData(nameof(InvalidDtos))]
+    //public void Delete_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
+    //{
+    //    //Arrange
 
-//    [Theory]
-//    [MemberData(nameof(InvalidDtos))]
-//    public void Delete_ShouldThrowException_WhenDtoIsInvalid(UserDto newUser, string errorMessage)
-//    {
-//        //Arrange
-        
-//        //Act
-//        //var action = _sut.Invoking(x => _sut.Delete(newUser));
+    //    //Act
+    //    //var action = _sut.Invoking(x => _sut.Delete(newUser));
 
-//        //// Assert
-//        //action.Should()
-//        //    .Throw<CustomValidationException>()
-//        //    .WithMessage(errorMessage);
-//    }
-//}
+    //    //// Assert
+    //    //action.Should()
+    //    //    .Throw<CustomValidationException>()
+    //    //    .WithMessage(errorMessage);
+    //}
+}
