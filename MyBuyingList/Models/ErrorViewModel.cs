5050e2b8a22a54d8859c765d85cf0cf2cf92d8df
Marcellus Farias <marcelluscfarias@gmail.com>
2023-10-17 16:00:45 -0300

    Deleted WebApp files (since it is an API currently).

--- MyBuyingList/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MyBuyingList.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
