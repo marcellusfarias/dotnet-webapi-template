0558f2845e81365c6c57458263804d0446b7e37a
Marcellus Farias <marcelluscfarias@gmail.com>
2024-01-08 15:10:50 -0300

    WIP: added ValueObject for EmailAddress

---
+++ MyBuyingList.Domain/ValueObjects/EmailAddress.cs
@@ -0,0 +1,29 @@
+﻿using MyBuyingList.Domain.Exceptions;
+using System.Text.RegularExpressions;
+
+namespace MyBuyingList.Domain.ValueObjects;
+
+// TODO: replace "string" Email for this ValueObject
+public class EmailAddress : ValueObject
+{
+    public string Address { get; init; }
+
+    public EmailAddress(string emailAddress)
+    {
+        if (!IsValid(emailAddress))
+            throw new InvalidValueException();
+
+        Address = emailAddress;
+    }
+
+    public static bool IsValid(string emailAddress)
+    {
+        Regex rxEmail = new Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        return rxEmail.IsMatch(emailAddress);
+    }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Address;
+    }
+}
